abfc92173fd44dabeafa059be63f16163cdfa0ff
MattKoRn <mattkorn2@gmail.com>
2026-07-11 09:56:14 +1000

    Activate neural non-map UI restrictions

--- gateway_to_caen/enhanced_ui.py
+++ gateway_to_caen/enhanced_ui.py
@@ -1,4 +1,4 @@
 """Loaded enhanced ui v6 implementation."""
 from ._source_loader import execute_parts
-execute_parts(globals(), "enhanced_ui_v6", 7)
+execute_parts(globals(), "enhanced_ui_v6", 8)
 del execute_parts
