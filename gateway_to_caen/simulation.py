34c5716f06bec82ab066b303d186b9222c840392
MattKoRn <mattkorn2@gmail.com>
2026-07-11 05:33:19 +1000

    Activate protected deployment and wreck collision pass

--- gateway_to_caen/simulation.py
+++ gateway_to_caen/simulation.py
@@ -1,4 +1,4 @@
 """Loaded simulation implementation."""
 from ._source_loader import execute_parts
-execute_parts(globals(), "simulation", 3)
+execute_parts(globals(), "simulation", 4)
 del execute_parts
