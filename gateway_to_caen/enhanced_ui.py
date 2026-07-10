ce231261d82d57bb0863e69431a909e2c226e03a
MattKoRn <mattkorn2@gmail.com>
2026-07-11 06:21:59 +1000

    Activate v0.6.1 presentation

--- gateway_to_caen/enhanced_ui.py
+++ gateway_to_caen/enhanced_ui.py
@@ -1,4 +1,4 @@
 """Loaded enhanced ui v6 implementation."""
 from ._source_loader import execute_parts
-execute_parts(globals(), "enhanced_ui_v6", 4)
+execute_parts(globals(), "enhanced_ui_v6", 5)
 del execute_parts
