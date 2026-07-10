6a3e622556223763d8b4947023ec2f6f88696a4c
MattKoRn <mattkorn2@gmail.com>
2026-07-11 06:34:48 +1000

    Activate neural UI navigation

--- gateway_to_caen/neural_cursor.py
+++ gateway_to_caen/neural_cursor.py
@@ -1,4 +1,4 @@
 """Loaded neural cursor v6 implementation."""
 from ._source_loader import execute_parts
-execute_parts(globals(), "neural_cursor_v6", 2)
+execute_parts(globals(), "neural_cursor_v6", 3)
 del execute_parts
